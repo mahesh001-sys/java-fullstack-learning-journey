311c476387b016ef081811f6a9cc7a132847e1eb
Banoth Mahesh Kumar <maheshbanoth057@gmail.com>
2026-06-17 18:42:54 +0530

    Comment out example input statements
    Commented out example input statements for clarity.

--- src/Basics/InputOutput.java
+++ src/Basics/InputOutput.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class InputOutput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        /*
+        int num = sc.nextInt();        // Integer
+        double d = sc.nextDouble();    // Decimal number
+        char ch = sc.next().charAt(0); // Character
+        String str = sc.next();        // Single word
+        String line = sc.nextLine();   // Full sentence */
 
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
